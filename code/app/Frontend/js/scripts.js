ca91ebd28761bc314ad219990ec4d9385d6ab5b5
Soulaimene <91623314+Soulaimene@users.noreply.github.com>
2023-12-25 13:37:17 +0100

    update login

--- code/app/Frontend/js/scripts.js
+++ code/app/Frontend/js/scripts.js
@@ -1,13 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
+    const usernameInput = document.getElementById('username');
+    const passwordInput = document.getElementById('password');
+    const errorContainer = document.getElementById('error-container');
+    const spinner = document.getElementById('spinner');
     document.getElementById('loginForm').addEventListener('submit', function (event) {
         event.preventDefault();
 
-        const username = document.getElementById('username').value;
-        const password = document.getElementById('password').value;
-
-        // Determine the role based on user selection
-
-
+        const username = usernameInput.value;
+        const password = passwordInput.value;
+        spinner.style.display = 'flex';
         // Make an HTTP request using Axios
         axios.post("https://lifeguardian.local/lifeguardian/api/oauth2/login", {
             grand_type: 'PASSWORD',
@@ -16,26 +17,43 @@ document.addEventListener('DOMContentLoaded', function () {
         })
             .then(response => {
                 // Handle successful response
+                spinner.style.display = 'none';
                 const { accessToken, refreshToken } = response.data;
 
                 // Save tokens in cookies
                 document.cookie = `accessToken=${accessToken}; path=/`;
                 document.cookie = `refreshToken=${refreshToken}; path=/`;
-                console.log("Connected")
-                // window.location.href = './index.html';
+
+                window.location.href = './dashboard.html';
             })
             .catch(error => {
+                spinner.style.display = 'none';
                 // Handle error
                 if (error.response) {
                     console.error(error.response.data);
                     console.error(error.response.status);
                     console.error(error.response.headers);
+
+                    // Display error container with red background and opacity
+                    errorContainer.style.display = 'flex';
                 } else if (error.request) {
                     console.error(error.request);
                 } else {
                     console.error('Error', error.message);
                 }
+            })
+            .finally(() => {
+                // Hide the spinner covering the entire page when the request is complete
+                spinner.style.display = 'none';
             });
+    });
 
+    // Add event listeners to hide the error container when typing starts
+    usernameInput.addEventListener('input', function () {
+        errorContainer.style.display = 'none';
+    });
+
+    passwordInput.addEventListener('input', function () {
+        errorContainer.style.display = 'none';
     });
 });
